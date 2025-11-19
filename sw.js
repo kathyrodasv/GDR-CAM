3d63596f190e135d66056ca300cdb49d911e5f4c
kathyrodasv <rodaskathy21@gmail.com>
2025-11-19 16:52:49 -0500

    main
    acceso P835

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = 'gdr-cam-v68.6.4';
-const STATIC_CACHE_NAME = 'gdr-cam-static-v68.6.4';
-const RUNTIME_CACHE_NAME = 'gdr-cam-runtime-v68.6.4';
+const CACHE_NAME = 'gdr-cam-v68.6.5';
+const STATIC_CACHE_NAME = 'gdr-cam-static-v68.6.5';
+const RUNTIME_CACHE_NAME = 'gdr-cam-runtime-v68.6.5';
 
 const urlsToCache = [
   './',
